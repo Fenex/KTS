2c51d3ea606cb7370422c2e927c9be9f4aa08c75
Vitaly Busko <vitbusko@gmail.com>
2013-04-06 05:28:10 +0700

    ver 14303; edit userjs QuickVocsStart
    added links: qual & user's stat

--- userjs/QuickVocsStart.user.js
+++ userjs/QuickVocsStart.user.js
@@ -1,117 +1,96 @@
 ﻿// ==UserScript==
-// @name           QuickVocsStart_CodeVersion
+// @name           QuickVocsStart
 // @namespace      fnx
 // @include        http://klavogonki.ru/vocs/*
 // @author         Fenex
 // @author-page    http://klavogonki.ru/profile/82885/
-// @version        2.8
+// @version        5.0 KTS
 // @icon           http://www.gravatar.com/avatar.php?gravatar_id=d9c74d6be48e0163e9e45b54da0b561c&r=PG&s=48&default=identicon
 // ==/UserScript==
-function userCode() {
-	var button_link = new Array();
-	for(i=0;i<4;i++) {
-		button_link[i] = new Object();
-	}
-	
-	//*************************************************************************************************//
-	//                       Ниже описываются настройки ссылок, их можно править:                      //
-	//   ------      ------      ------      ------      ------      ------      ------      ------    //
-	//name - то, что будет отображаться на странице;                                                   //
-	//type [practice, private, normal] - тип заезда [одиночный, заезд с друзьями, открытый];           //
-	//level_from [1,2,3,4,5,6,7,8,9] - огнаничение на вход "снизу", [1 - новички, ..., 9 - экстракиберы];//
-	//level_to [1,2,3,4,5,6,7,8,9] - огнаничение на вход "сверзу", [1 - новички, ..., 9 - экстракиберы]; //
-	//timeout [1, 2, ..., 180] - таймаут, в секундах;                                                  //
-	//qual - [true, false] - режим игры [квалификация, стандартный режим];                             //
-	//enabled [true, false] - включение дополнительны кнопок [включить, отключить].                    //
-	//*************************************************************************************************//
-	
-	//главная кнопка, включена всегда.
-	button_link[0].name = "одиночный, 5 сек";
-	button_link[0].type = "practice";
-	button_link[0].level_from = "1";
-	button_link[0].level_to = "9";
-	button_link[0].timeout = "5"
-	button_link[0].qual = false;
-		
-	//дополнительные кнопки;
-	//для включения "enabled = true;"
-	
-	//Кнопка номер раз.	
-	button_link[1].enabled = true;
-	button_link[1].name = "открытый, 10 сек";
-	button_link[1].type = "normal";
-	button_link[1].level_from = "1";
-	button_link[1].level_to = "9";
-	button_link[1].timeout = "10"
-	button_link[1].qual = false;
-	
-	//Кнопка номер два.
-	button_link[2].enabled = true;
-	button_link[2].name = "открытый, 20 сек";
-	button_link[2].type = "normal";
-	button_link[2].level_from = "1";
-	button_link[2].level_to = "9";
-	button_link[2].timeout = "20"
-	button_link[2].qual = false;
-	
-	//Кнопка номер три.
-	button_link[3].enabled = true;
-	button_link[3].name = "дружеский, 10 сек";
-	button_link[3].type = "private";
-	button_link[3].level_from = "1";
-	button_link[3].level_to = "9";
-	button_link[3].timeout = "10"
-	button_link[3].qual = false;
-	
-	//А дальше, клавогонщик, замри!!!
-	
-	//----------------------------------------------------------------//
-	//            Дальше код не трогать, а то сломаете...             //
-	//----------------------------------------------------------------//
-	
-	return button_link;
-}
-
-function init_QuickScript() {
-	if(!(/^http:\/\/klavogonki.ru\/vocs\/\d+/.test(location.href)))
-		return;
-	var f = true;
-	var txt = "";
-	var elem = document.getElementsByClassName('user-title')[0].getElementsByClassName('remove')[0];
-	if(!elem)
-		return;
-	elem.parentNode.setAttribute('class', 'links');
-	var vocid = location.href.match(/\d+/);
-	var m = userCode();
-	for(i=0;i<4;i++)
-		m[i].qual = m[i].qual ? "&qual=1" : "";
-	var href = "http://klavogonki.ru/create/?gametype=voc&voc="+vocid+"&type="+m[0].type+"&level_from="+m[0].level_from+"&level_to="+m[0].level_to+"&timeout="+m[0].timeout+m[0].qual+"&submit=Начать";
-	var e = document.createElement('a');
-	e.setAttribute('href', href);
-	e.innerHTML = '<span>'+m[0].name+'</span>';
-	elem.parentNode.insertBefore(e, elem.nextSibling);
-	for(i=1;i<4;i++) {
-		if(!m[i].enabled)
-			continue;
-		f = false;
-		txt += "<a href='http://klavogonki.ru/create/?gametype=voc&voc="+vocid+"&type="+m[i].type+"&level_from="+m[i].level_from+"&level_to="+m[i].level_to+"&timeout="+m[i].timeout+m[i].qual+"&submit=Начать'><span>"+m[i].name+"</span></a>";
-	}
-	if(f)
-		return;
-	e = document.createElement('td');
-	e.setAttribute('class', 'links');
-	e.setAttribute('id', 'td_QSUS');
-	e.innerHTML = txt;
-	elem.parentNode.parentNode.insertBefore(e, elem.parentNode.nextSibling);
-	
-	var s = document.createElement('style');
-	s.innerHTML = '.user-title .links a{margin: 7px 0px 0px 12px !important;}';
-	document.body.appendChild(s);
-}
-
 if(!document.getElementById('KTS_QuickVocsStart')) {
-	init_QuickScript();
+	(function() {
+		var voc = location.href.match(/^http:\/\/klavogonki.ru\/vocs\/([\d]+)/);
+		if(!voc)
+			return;
 		
+		function getIDofUser() {
+			var tmp = document.getElementsByClassName('user-block')[0];
+			if(!tmp)
+				return 0;
+			return tmp.getElementsByClassName('profile')[0].href.match(/profile\/([\d]+)/)[1];
+		}
+		
+		function generateLink(param) {
+			var href = 'http://klavogonki.ru/create/?submit=1&gametype=voc';
+			if(param.qual)
+				href += '&qual=on';
+			href += '&voc=' + param.vocid;
+			href += '&type=' + param.type;
+			href += '&level_from=' + param.level_from;
+			href += '&level_to=' + param.level_to;
+			href += '&timeout=' + param.timeout;
+			return href;
+		}
+		
+		function getBlock(block) {
+			var td = document.createElement('td');
+			td.setAttribute('class', 'links');
+			td.setAttribute('style', 'padding-left:10px;');
+			if(block) {
+				td.innerHTML = '<a href="' + generateLink({
+											vocid: vocid,
+											type: "practice",
+											level_from: 1,
+											level_to: 9,
+											timeout: 5,
+											qual: false
+										}) + '"><span>Одиночный, 5 сек</span></a>';
+				td.innerHTML += '<a href="' + generateLink({
+											vocid: vocid,
+											type: "normal",
+											level_from: 1,
+											level_to: 9,
+											timeout: 10,
+											qual: false
+										}) + '"><span>Открытый, 10 сек</span></a>';
+				td.innerHTML += '<a href="' + generateLink({
+											vocid: vocid,
+											type: "normal",
+											level_from: 1,
+											level_to: 9,
+											timeout: 20,
+											qual: false
+										}) + '"><span>Открытый, 20 сек</span></a>';
+			} else {
+				td.innerHTML = '<a href="' + generateLink({
+											vocid: vocid,
+											type: "private",
+											level_from: 1,
+											level_to: 9,
+											timeout: 10,
+											qual: false
+										}) + '"><span>Дружеский, 10 сек</span></a>';
+				td.innerHTML += '<a href="' + generateLink({
+											vocid: vocid,
+											type: "practice",
+											level_from: 1,
+											level_to: 9,
+											timeout: 10,
+											qual: true
+										}) + '"><span>Квалификация, 10 сек</span></a>';
+				td.innerHTML += '<a href="http://klavogonki.ru/profile/'+userid+'/stats/?gametype=voc-'+vocid+'"><span>Статистика</span></a>';
+			}
+			return td;
+		}
+		
+		var userid = getIDofUser();
+		var vocid = voc[1];
+		var elem = document.getElementsByClassName('user-title')[0].getElementsByClassName('remove')[0].parentNode;
+		elem.setAttribute('class', 'links');
+		elem.parentNode.insertBefore(getBlock(0), elem.nextSibling);
+		elem.parentNode.insertBefore(getBlock(1), elem.nextSibling);
+	})();
+	
 	var tmp_elem = document.createElement('div');
 	tmp_elem.id = 'KTS_QuickVocsStart';
 	tmp_elem.style.display = 'none';
