7d2c53656d840ea08813e1d224ae0eea8f360a50
Vitaly Busko <vitbusko@gmail.com>
2013-06-28 09:25:45 +0700

    ver 14503; update opt.dat

--- loadmain.js
+++ loadmain.js
@@ -10,7 +10,7 @@ function rememberSettings() {
 
 var userid = false;
 var KlavoTools = new Object();
-var KTS_ver = 14502;
+var KTS_ver = 14503;
 var kco = false;
 var w_php = true;
 var KTS_timeout = 90 * 1000; //1min 30sec
