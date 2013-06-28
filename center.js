4505ba9d6fb1b4cb0a27bd7a550449517a4aa3fe
Vitaly Busko <vitbusko@gmail.com>
2013-06-28 15:56:09 +0700

    ver 14504; fix enabler klavoevents.user.js

--- center.js
+++ center.js
@@ -81,7 +81,7 @@ function insertScriptsOnTab(tabId, url) {
 	}
 	
 	if(/klavogonki\.ru/.test(url)) {
-		//if(KlavoTools.userjs.KlavoEvents)
+		if(KlavoTools.userjs.KlavoEvents)
 			chrome.tabs.executeScript(tabId, {file: 'userjs/KlavoEvents.user.js'});
 		if(KlavoTools.userjs.DailyScores)
 			chrome.tabs.executeScript(tabId, {file: 'userjs/DailyScores.user.js'});
