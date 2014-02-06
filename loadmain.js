a378ea0fdf2e4f72a337815275bb2b215cde39eb
Vitaliy <vitbusko@gmail.com>
2014-02-06 17:05:53 +0700

    ver 15105; fix competitions.js

--- loadmain.js
+++ loadmain.js
@@ -10,7 +10,7 @@ function rememberSettings() {
 
 var userid = false;
 var KlavoTools = new Object();
-var KTS_ver = 15104;
+var KTS_ver = 15105;
 var kco = false;
 var w_php = true;
 var KTS_timeout = 90 * 1000; //1min 30sec
